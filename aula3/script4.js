45804fcb3fb2ea14fe9d18a2aac75a296a088cae
Auto Logon <autologon@CSED.LAB>
2022-09-08 21:38:34 -0300

    aula 3 dada

--- aula3/script4.js
+++ aula3/script4.js
@@ -19,5 +19,17 @@ let i = 1;
 
 do {
     console.log(`=> ${i}`);
-    i++
+    i++;
 } while (i <= 10);
+
+console.log("\n========================\n");
+
+let line = "";
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= i; j++) {
+        line += "*";
+    }
+    console.log(line);
+    line = "";
+}
