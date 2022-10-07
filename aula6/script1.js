7e3767122077b9b0eb41e7f31657733a2b5cf4f2
Auto Logon <autologon@CSED.LAB>
2022-10-06 21:00:06 -0300

    aula 6 dada

---
+++ aula6/script1.js
@@ -0,0 +1,57 @@
+let nome = "Pedro";
+
+/* 
+
+Estrutura condicional if
+
+if(nome == "Juca"){
+    // bloco de código 1
+}else if(nome == "Pedro"){
+    // bloco de código 2
+}else{
+    // bloco de código 3
+}
+
+Estrutura de repetição for
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+Estrutura de repetição while
+
+let i = 1;
+while(i <= 10){
+    console.log(i);
+    i++;
+}
+
+Estrutura de repetição do while
+
+let i = 1;
+do{
+    console.log(i);
+    i++;
+}while(i <= 10);
+
+
+Estrutura de repetição for of
+
+let nomes = ["Juca", "Pedro", "Maria"];
+
+for (let nome of nomes) {
+    console.log(nome);
+}
+
+Estrutura de repetição for in
+
+let pessoa = {
+    nome: "Juca",
+    idade: 20,
+    sexo: "M"
+};
+
+for (let chave in pessoa) {
+    console.log(chave + ": " + pessoa[chave]);
+}
+*/
